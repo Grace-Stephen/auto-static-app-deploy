15eaa11e436bf9e89b1e2291bdb9350d314202ec
Grace-Stephen <gracestephen24@gmail.com>
2025-08-06 21:31:00 +0100

    updated code for infra deployment

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region = var.region
 }
+
+module "infra" {
+  source = "./infra"
+
+  account_id = var.account_id
+  vpc_cidr = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones = var.availability_zones
+  instance_type = var.instance_type
+  ami = var.ami
+  ecr_repo_name = var.ecr_repo_name
+  domain_name = var.domain_name
+  subdomain = var.subdomain
+}
