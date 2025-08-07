8c5319bef7abcf55b69c111c70feb7873f4b2370
Grace-Stephen <gracestephen24@gmail.com>
2025-08-07 22:36:53 +0100

    updated code for infra deployment

--- infra/acm.tf
+++ infra/acm.tf
@@ -17,15 +17,15 @@ resource "aws_acm_certificate" "ssl_cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.ssl_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-}
+# resource "aws_route53_record" "cert_validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.ssl_cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       type   = dvo.resource_record_type
+#       record = dvo.resource_record_value
+#     }
+#   }
+# }
 #   # zone_id = var.route53_zone_id
 #   name    = each.value.name
 #   type    = each.value.type
