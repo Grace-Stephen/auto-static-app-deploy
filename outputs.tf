fc2c87eceb3665310ee1e64beb07aeb1b3bc5460
Grace-Stephen <gracestephen24@gmail.com>
2025-08-08 17:04:28 +0100

    updated code for infra deployment

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "ecr_repo_url" {
-  description = "URL of the created ECR repository"
-  value       = aws_ecr_repository.app.repository_url
+  value = module.infra.ecr_repo_url
 }
 
+
 output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
+  value = module.infra.alb_dns_name
 }
